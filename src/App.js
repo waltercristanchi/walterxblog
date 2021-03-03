7e69c8b1b09d1aedd3e51bba8ba59bc6477a3989
Walter <waltercristanchi21@gmail.com>
2021-03-03 19:36:16 -0300

    fixing imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Contador from './components/Contador.jsx';
-import Evento from './components/Evento.jsx';
-import Parrafo from './components/Parrafo.jsx';
+
 function App() {
   return (
     <div>
